61825f4dd94a30d79511044e5eb13e290e845064
Jahir Hossain <j.hossain@reply.de>
2019-05-05 14:17:32 +0200

    Create First fake test

---
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Tests\AppBundle\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class DinosaurTest extends TestCase
+{
+    public function testIfEverythingWorksProperly()
+    {
+        $this->assertTrue(true);
+    }
+}
