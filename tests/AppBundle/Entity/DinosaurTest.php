d465b1b6d7be892f9c41b86e5ea1b3bbecc4fee2
Jahir Hossain <j.hossain@reply.de>
2019-05-05 14:40:10 +0200

    create test for setting Dinosaur length

--- tests/AppBundle/Entity/DinosaurTest.php
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tests\AppBundle\Entity;
 
+use AppBundle\Entity\Dinosaur;
 use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
@@ -8,5 +9,16 @@ class DinosaurTest extends TestCase
     public function testIfEverythingWorksProperly()
     {
         $this->assertTrue(true);
+    }
+
+    public function testSettingLength()
+    {
+        $dinosaur = new Dinosaur();
+
+        $this->assertSame(0, $dinosaur->getLength());
+
+        $dinosaur->setLength(9);
+
+        $this->assertSame(9, $dinosaur->getLength());
     }
 }
