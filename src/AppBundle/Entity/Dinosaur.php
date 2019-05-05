e48d385c842692fb5de0b724904be89002ce3e85
Jahir Hossain <j.hossain@reply.de>
2019-05-05 16:55:06 +0200

    create get specification function

--- src/AppBundle/Entity/Dinosaur.php
+++ src/AppBundle/Entity/Dinosaur.php
@@ -14,6 +14,22 @@ class Dinosaur
      * @ORM\Column(type="integer")
      */
     private $length = 0;
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private $genus;
+    /**
+     * @var bool
+     * @ORM\Column(type="boolean")
+     */
+    private $isCarnivorous;
+
+    public function __construct(string $genus ='Unknown', bool $isCarnivorous = false)
+    {
+        $this->genus = $genus;
+        $this->isCarnivorous = $isCarnivorous;
+    }
 
     /**
      * @return integer
@@ -31,6 +47,9 @@ class Dinosaur
         $this->length = $length;
     }
 
-
-
+    public function getSpecification()
+    {
+        return sprintf('The %s %scarnivorous dinosaur is %d meters long',
+            $this->genus, $this->isCarnivorous ? '' : 'non-', $this->getLength());
+    }
 }
