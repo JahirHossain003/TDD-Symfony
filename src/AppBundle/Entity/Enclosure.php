f7d40b4b4440b19bd28bf21d239faeaca3fd0e02
Jahir Hossain <j.hossain@reply.de>
2019-05-07 11:25:42 +0200

    add security to enclosure

--- src/AppBundle/Entity/Enclosure.php
+++ src/AppBundle/Entity/Enclosure.php
@@ -4,6 +4,7 @@
 namespace AppBundle\Entity;
 
 
+use AppBundle\Exception\DinosaursAreRunningRampantException;
 use AppBundle\Exception\NotABuffetException;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
@@ -20,9 +21,20 @@ class Enclosure
      */
     private $dinosaurs;
 
-    public function __construct()
+    /**
+     * @var ArrayCollection|Security[]
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Security", mappedBy="enclosure", cascade={"persist"})
+     */
+    private $securities;
+
+    public function __construct(bool $addBasicSecurity = false)
     {
         $this->dinosaurs = new ArrayCollection();
+        $this->securities = new ArrayCollection();
+
+        if ($addBasicSecurity) {
+            $this->addSecurity(new Security('Fence',true,$this));
+        }
     }
 
     /**
@@ -34,21 +46,54 @@ class Enclosure
     }
 
     /**
+     * @return ArrayCollection
+     */
+    public function getSecurities(): ArrayCollection
+    {
+        return $this->securities;
+    }
+
+    /**
      * @param Dinosaur $dinosaur
      * @throws NotABuffetException
+     * @throws DinosaursAreRunningRampantException
      */
     public function addDinosaur(Dinosaur $dinosaur)
     {
         if (!$this->canAddDinosaur($dinosaur)) {
-           throw new NotABuffetException();
+            throw new NotABuffetException();
+        }
+
+        if (!$this->isSecurityActive()) {
+            throw new DinosaursAreRunningRampantException('Are you carzzy ?!?');
         }
 
         $this->dinosaurs[] = $dinosaur;
     }
 
+    public function isSecurityActive(): bool
+    {
+        /** @var Security $security */
+        foreach ($this->getSecurities() as $security) {
+            if ($security->getIsActive()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     private function canAddDinosaur(Dinosaur $dinosaur)
     {
         return count($this->getDinosaurs()) == 0 || $this->getDinosaurs()->first()->isCarnivorous() == $dinosaur->isCarnivorous();
+    }
+
+    /**
+     * @param Security $security
+     */
+    private function addSecurity(Security $security)
+    {
+        $this->securities[] = $security;
     }
 
 }
--- tests/AppBundle/Entity/EnclosureTest.php
+++ tests/AppBundle/Entity/EnclosureTest.php
@@ -6,6 +6,7 @@ namespace Tests\AppBundle\Entity;
 
 use AppBundle\Entity\Dinosaur;
 use AppBundle\Entity\Enclosure;
+use AppBundle\Exception\DinosaursAreRunningRampantException;
 use AppBundle\Exception\NotABuffetException;
 use PHPUnit\Framework\TestCase;
 
@@ -21,18 +22,30 @@ class EnclosureTest extends TestCase
 
     public function testItAddsDinosaur()
     {
-        $enclosure = new Enclosure();
+        $enclosure = new Enclosure(true);
         $enclosure->addDinosaur(new Dinosaur());
         $enclosure->addDinosaur(new Dinosaur());
 
         $this->assertCount(2,$enclosure->getDinosaurs());
     }
 
+    /**
+     * @expectedException  AppBundle\Exception\NotABuffetException
+     */
     public function testItTrowsExceptionWhenPuttingCarnivorousAndNonCarnivorousTogether()
     {
-        $enclosure = new Enclosure();
+        $enclosure = new Enclosure(true);
         $enclosure->addDinosaur(new Dinosaur());
-        $this->expectException(NotABuffetException::class);
         $enclosure->addDinosaur(new Dinosaur('Velociraptor',true));
+    }
+
+    public function testItDoesNotAllowToAddDinosaurToUnsecureEnclosures()
+    {
+        $enclosure = new Enclosure();
+
+        $this->expectException(DinosaursAreRunningRampantException::class);
+        $this->expectExceptionMessage('Are you carzzy ?!?');
+
+        $enclosure->addDinosaur(new Dinosaur());
     }
 }
